76da89960d8e0b405d0499a04ae676464c999b2e
Adam Austin <aausti23@myune.edu.au>
2020-09-17 21:35:26 +1000

    react base builder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {MyComponent} from './components'; // import componenets
+
+import styles from './App.module.css';
+
+const App = () => {
+    return (
+        <>
+            <h1 className={styles.app}>App</h1>
+            <MyComponent />
+        </>
+    )
 }
 
 export default App;
---
+++ src/components/MyComponent/MyComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './MyComponent.module.css';
+
+const MyComponent = () => {
+    return (
+        <h1 className={styles.title}>My component</h1>
+    )
+}
+
+export default MyComponent;
